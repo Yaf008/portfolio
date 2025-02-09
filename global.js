0b5b00621d378ce49e64a94f652d27c83eacc4a1
Nicole Fang <yaf008@ucsd.edu>
2025-02-08 16:52:07 -0800

    Update global.js

--- global.js
+++ global.js
@@ -225,7 +225,29 @@ let arc = arcGenerator({
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
 let data = [1, 2];
-let sliceGenerator = d3.pie();
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
 
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = ['gold', 'purple'];
+
+
+arcs.forEach((arc, i) => {
+  d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr("fill", colors[i]); 
+});
