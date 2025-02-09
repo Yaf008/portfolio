f973368eee24948d81184387022b3248e5a38ba2
Nicole Fang <yaf008@ucsd.edu>
2025-02-08 19:49:50 -0800

    updated

--- global.js
+++ global.js
@@ -216,6 +216,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
+llet arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
 let data = [
   { value: 1, label: 'apples' },
   { value: 2, label: 'oranges' },
@@ -224,6 +233,7 @@ let data = [
   { value: 5, label: 'limes' },
   { value: 5, label: 'cherries' },
 ];
+
 
 let sliceGenerator = d3.pie().value((d) => d.value);
 
@@ -249,5 +259,14 @@ let colors = d3.scaleOrdinal(d3.schemeTableau10);
 arcData.forEach((d, i) => {
   d3.select('svg')
   .attr('d', arcGenerator(d)) // 这里 d 已经包含 startAngle 和 endAngle
-    .attr("fill", colors(i));
+  .attr("fill", colors(i));
 });
+
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend.append('li')
+        .attr('class', 'legend-item')
+        .html(`<span class="swatch" style="background-color: ${colors(idx)};"></span> ${d.label} <em>(${d.value})</em>`);
+});
+
