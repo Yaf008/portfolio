50ea1500e3c7a55a6ac98596c5695bb9e0077110
Nicole Fang <yaf008@ucsd.edu>
2025-02-08 16:35:35 -0800

    updated

--- global.js
+++ global.js
@@ -214,4 +214,13 @@ document.addEventListener("DOMContentLoaded", () => {
     .attr("fill", "red");
 });
 
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+
 
