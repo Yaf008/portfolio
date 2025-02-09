cb0f703e55da49148e25eb9dec200e12e4081cde
Nicole Fang <yaf008@ucsd.edu>
2025-02-08 18:56:58 -0800

    Update global.js

--- global.js
+++ global.js
@@ -225,7 +225,14 @@ let arc = arcGenerator({
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 
 let total = 0;
 
@@ -256,6 +263,8 @@ arcs.forEach((arc, i) => {
 
 
 //step 2
+
+let sliceGenerator = d3.pie().value((d) => d.value);
 
 let legend = d3.select('.legend');
 
