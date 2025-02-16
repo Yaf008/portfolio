7113e101c8643190c57800197ec7253cfe1b81f3
Nicole Fang <yaf008@ucsd.edu>
2025-02-15 20:51:45 -0800

    1.2

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 let data = [];
+let commits = [];
 
 async function loadData() {
   data = await d3.csv('loc.csv', (row) => ({
@@ -10,9 +11,46 @@ async function loadData() {
     datetime: new Date(row.datetime),
   }));
 
-  console.log(data);
+  processCommits();
+  console.log(commits);
 }
+
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
+
+
+function processCommits() {
+    commits = d3.groups(data, (d) => d.commit)
+      .map(([commit, lines]) => {
+        let first = lines[0]; // Get the first row of the commit
+  
+        let { author, date, time, timezone, datetime } = first;
+  
+        let ret = {
+          id: commit,
+          url: 'https://github.com/YOUR_REPO/commit/' + commit, // Replace with your warehouse
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          // Calculation hours (e.g. 14:30 = 14.5)
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          // Count the number of rows modified by the commit
+          totalLines: lines.length,
+        };
+  
+        
+        Object.defineProperty(ret, 'lines', {
+          value: lines,
+          writable: false, 
+          enumerable: false, 
+          configurable: false, 
+        });
+  
+        return ret;
+      });
+  }
+  
