3f7a3a4e8b9ea1c1468b3b3c2ea632c62bd51813
Nicole Fang <yaf008@ucsd.edu>
2025-02-15 18:04:07 -0800

    meta update

--- global.js
+++ global.js
@@ -5,6 +5,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'CV/', title: 'Resume' },
   { url: 'contact/', title: 'Contact' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/Yaf008', title: 'GitHub' }
 ];
 
